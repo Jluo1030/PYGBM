7f046bb4857042e34703f2569d6e133de8c554fe
Jluo1030 <jyluo1030@outlook.com>
2024-11-08 07:49:22 +0800

    Add .gitignore and pygbm folder

---
+++ pygbm/gbm_simulator.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+Class GBMSimulator:
+
+    def __init__(self, y_0, mu, sigma):
+        self.y_0 = y_0
+        self.mu = mu
+        self.sigma = sigma
+
+    def simulate_path(T, N):
+        t = np.linspace(0,T,N)
+        y = np.zeros([1,N])
+        time_interval = T/N
+        y[0][0] = self.y_0
+
+        for i in range(1,N):
+            y[0][i] = y[0][i-1] * (1 + self.miu * time_interval + self.sigma * np.random.normal(0, time_interval, 1))
+        
+        return t,y
+    
